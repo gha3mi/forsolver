7011ea174ca2bdb682adafa6873d09c6252c78b5
Seyed Ali Ghasemi <info@gha3mi.com>
2023-06-22 13:38:22 +0200

    Added newton method

--- src/forsolver.f90
+++ src/forsolver.f90
@@ -12,7 +12,8 @@ module forsolver
 
    !===============================================================================
    interface solver
-      procedure :: dgels_rel ! Interface for the pinverse_rel function
+      procedure :: dgels_rel
+      procedure :: newton_rel
    end interface
    !===============================================================================
 
@@ -71,6 +72,78 @@ contains
       ! deallocate workspace
       deallocate(work)
    end function dgels_rel
+   !===============================================================================
+
+   !===============================================================================
+   !> author: Seyed Ali Ghasemi
+   impure function newton_rel(F, dFdx, x0, tol, maxit) result(x_sol)
+      use kinds
+      implicit none
+
+      interface
+         impure function Fun(x)
+            use kinds
+            real(rk), intent(in) :: x
+            real(rk)             :: Fun
+         end function Fun
+
+         impure function dFun(x)
+            use kinds
+            real(rk), intent(in) :: x
+            real(rk)             :: dFun
+         end function dFun
+      end interface
+
+      procedure(Fun)  :: F
+      procedure(dFun) :: dFdx
+
+      real(rk), intent(in)  :: x0, tol
+      integer,  intent(in)  :: maxit
+      real(rk)              :: x_sol
+      real(rk)              :: x, xnp
+      real(rk)              :: F_val, dFdx_val, Krit
+      integer               :: it
+      logical               :: convergenz
+
+      ! Variable declaration
+      x          = x0
+      xnp        = x0
+      it         = 0
+      convergenz = .false.
+
+      write(*, '(a)') '-----------------------------------------------'
+      write(*, '(a)') 'maxit             x0                   tol'
+      write(*, '(i3, 10x, f12.8, 10x, e12.4)') maxit, x0, tol
+      write(*, '(a)') '-----------------------------------------------'
+      write(*, '(a)') 'start newton'
+      write(*, '(a)') '-----------------------------------------------'
+      write(*, '(a)') 'it        xn           F(xn)        dF(xn)/dxn'
+
+      ! Main loop
+      do while (.not. convergenz .and. it < maxit)
+         F_val    = F(x)
+         dFdx_val = dFdx(x)
+         xnp      = x - F_val / dFdx_val
+
+         Krit = abs(F_val)
+
+         if (Krit <= tol) then
+            convergenz = .true.
+            x_sol      = x
+         end if
+
+         write(*, '(i3, f12.4, 4x, e12.4, 4x, e12.4)') it, x, F_val, dFdx_val
+         it = it + 1
+
+         x = xnp
+      end do
+
+      write(*, '(a)') '-----------------------------------------------'
+      write(*, '(a)') 'end newton'
+      write(*, '(a)') '-----------------------------------------------'
+      write(*, '(a, g0)') 'x_sol = ', x_sol
+
+   end function newton_rel
    !===============================================================================
 
 end module forsolver
---
+++ test/test2.f90
@@ -0,0 +1,39 @@
+module functions_module
+use kinds
+implicit none
+
+contains
+
+function F(x) result(F_val)
+   real(rk), intent(in) :: x
+   real(rk) :: F_val
+   F_val = 5_rk * x**3 + 8_rk * x - 5_rk
+end function F
+
+function dFdx(x) result(dFdx_val)
+   real(rk), intent(in) :: x
+   real(rk) :: dFdx_val
+   dFdx_val = 15_rk * x**2 + 8_rk
+end function dFdx
+
+end module functions_module
+
+
+
+program main
+   use kinds
+   use forsolver
+   use functions_module
+   implicit none
+   real(rk) :: x0, tol, x_sol
+   integer :: maxit
+
+   ! Variable declaration
+   x0    = 10.0_rk
+   tol   = 1e-8_rk
+   maxit = 100
+
+   x_sol = solver(F, dFdx, x0, tol, maxit)
+
+end program main
+
