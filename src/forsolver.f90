cfa8dfb4075887c50b7925b9689077e93de14c24
Seyed Ali Ghasemi <info@gha3mi.com>
2023-07-06 08:12:22 +0200

    Add dgesv and fix overwritten bug

--- src/forsolver.f90
+++ src/forsolver.f90
@@ -1,8 +1,8 @@
 module forsolver
 
-  ! This module provides functions and subroutines for 
-  ! solving linear systems and performing Newton's method.
-   
+   ! This module provides functions and subroutines for
+   ! solving linear systems and performing Newton's method.
+
    use :: kinds
 
    implicit none
@@ -13,12 +13,87 @@ module forsolver
 
    !===============================================================================
    interface solver
-      procedure :: dgels_rel
+      procedure :: solver_lin
       procedure :: newton_rel
    end interface
    !===============================================================================
 
 contains
+
+   !===============================================================================
+   !> author: Seyed Ali Ghasemi
+   pure function solver_lin(A, b, method) result(x)
+
+      ! inputs
+      real(rk),     dimension(:, :), contiguous, intent(in) :: A      ! input matrix A
+      real(rk),     dimension(:),    contiguous, intent(in) :: b      ! right-hand side matrix b
+      character(*), optional,                    intent(in) :: method
+
+      ! outputs:
+      real(rk), dimension(size(b))                       :: x    ! solution matrix x
+
+      if (present(method)) then
+         select case (method)
+          case ('gesv')
+            x = dgesv_rel(A, b)
+          case ('gels')
+            x = dgels_rel(A, b)
+         end select
+      else
+         if (size(A,1)==size(A,2)) then
+            x = dgesv_rel(A, b)
+         else 
+            x = dgels_rel(A, b)
+         end if
+      end if
+
+   end function solver_lin
+   !===============================================================================
+
+   !===============================================================================
+   !> author: Seyed Ali Ghasemi
+   pure function dgesv_rel(A, b) result(x)
+      ! inputs:
+      real(rk), dimension(:, :), contiguous, intent(in)  :: A    ! input matrix A
+      real(rk), dimension(:),    contiguous, intent(in)  :: b    ! right-hand side matrix b
+
+      ! outputs:
+      real(rk), dimension(size(b))                       :: x    ! solution matrix x
+
+      ! local variables
+      integer                                  :: info ! result info
+      integer                                  :: n, lda, ldb
+      integer,  dimension(size(A, 2))          :: ipiv
+      real(rk), dimension(size(A,1),size(A,2)) :: a_copy
+      real(rk), dimension(size(b))             :: b_copy
+
+      ! interface for dgels subroutine
+      interface
+         pure subroutine dgesv(fn, fnrhs, fa, flda, fipiv, fb, fldb, finfo)
+            use kinds
+            integer,  intent(in)    :: fn, fnrhs, flda, fldb
+            real(rk), intent(inout) :: fa(flda,*), fb(fldb,*)
+            integer,  intent(out)   :: finfo
+            integer,  intent(out)   :: fipiv(fn)
+         end subroutine dgesv
+      end interface
+
+      b_copy = b
+      a_copy = a
+
+      ! get dimensions
+      n    = size(A, 2)
+      lda  = max(1, n)
+      ldb  = max(1, n)
+
+      ! call dgels subroutine
+      call dgesv(n, 1, a_copy, n, ipiv, b_copy, n, info)
+
+      ! copy the solution matrix
+      x = b_copy
+
+   end function dgesv_rel
+   !===============================================================================
 
    !===============================================================================
    !> author: Seyed Ali Ghasemi
@@ -32,11 +107,12 @@ contains
       real(rk), dimension(size(b))                        :: x    ! solution matrix x
 
       ! local variables
-      integer                          :: info ! result info
-      integer                          :: m, n, lda, ldb, lwork
-      real(rk), allocatable            :: work(:)
-      real(rk)                         :: work1(1)
-      real(rk), dimension(size(b))     :: b_copy
+      integer                                  :: info ! result info
+      integer                                  :: m, n, lda, ldb, lwork
+      real(rk), allocatable                    :: work(:)
+      real(rk)                                 :: work1(1)
+      real(rk), dimension(size(A,1),size(A,2)) :: a_copy
+      real(rk), dimension(size(b))             :: b_copy
 
       ! interface for dgels subroutine
       interface
@@ -44,11 +120,13 @@ contains
             use kinds
             character(len=1), intent(in)    :: ftrans
             integer,          intent(in)    :: fm, fn, fnrhs, flda, fldb, flwork
-            real(rk),         intent(in)    :: fa(flda,*), fb(fldb,*), fwork(*)
+            real(rk),         intent(inout) :: fa(flda,*), fb(fldb,*)
+            real(rk),         intent(in)    :: fwork(*)
             integer,          intent(out)   :: finfo
          end subroutine dgels
       end interface
 
+      a_copy = a
       b_copy = b
 
       ! get dimensions
@@ -58,14 +136,14 @@ contains
       ldb  = max(1, max(m, n))
 
       ! calculate the optimal size of the work array
-      call dgels('n', m, n, 1, A, lda, b_copy, ldb, work1, -1, info)
+      call dgels('n', m, n, 1, a_copy, lda, b_copy, ldb, work1, -1, info)
 
       ! allocate work array
       lwork = nint(work1(1))
       allocate(work(lwork))
 
       ! call dgels subroutine
-      call dgels('n', m, n, 1, A, lda, b_copy, ldb, work, lwork, info)
+      call dgels('n', m, n, 1, a_copy, lda, b_copy, ldb, work, lwork, info)
 
       ! copy the solution matrix
       x = b_copy
